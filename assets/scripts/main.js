b7ecbd00baf1a4cebce5f7b8426d95e62f44b0f2
Andres Aguilar <andresyoshimar@gmail.com>
2018-02-09 21:02:39 -0600

    comenzando a jugar con javascript

---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+// console.log("Hola mundo");
+
+// Retorna todos los elementos encontrados en una lista
+let links = document.querySelectorAll("a");
+
+links.forEach(function(link){
+  console.log(link);
+});
